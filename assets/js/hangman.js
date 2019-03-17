fc8e45ffd4e74ced5ecdb69ead25d82fa8cd634b
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-03-17 18:46:55 -0500

    pushing to branch in js atm trying to generate UnderScores

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,2 +1,21 @@
-var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var answer = ["river"];
+// pseudo
+// Make an array of words
+var words = ["Charmander", "Magmar", "Flareon", "Ponyta"]
+var scramble = Math.floor(Math.random() * words.length);
+var scoreUnder = [];
+var iChooseYou = words[scramble];
+console.log(iChooseYou);
+// add or subtract underscores for length of words
+var generateUnderscore = () => {
+    for(var i = 0; i < iChooseYou.length; i++) {
+        scoreUnder.push('_');
+    }
+    return scoreUnder;
+}
+
+console.log(generateUnderscore());
+// input users guess
+// is guest right
+// if right
+// if wrong
+// wins loses letters guessed
